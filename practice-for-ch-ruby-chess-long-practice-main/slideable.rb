b8868bd0e9786debc5fc140f9ce0a6e8ed81c2d3
Andoniank <andoniank@gmail.com>
2023-08-09 11:15:16 -0700

    Add move_dirs in Slideable

--- practice-for-ch-ruby-chess-long-practice-main/slideable.rb
+++ practice-for-ch-ruby-chess-long-practice-main/slideable.rb
@@ -11,7 +11,28 @@ module Slideable
         DIAGONAL_DIRS
     end
 
-    def move_dirs
+    def moves(pos)
+        
+    end
 
+    def move_dirs(pos)
+        moves = []
+        straight_dirs.each do |x, y|
+            # while inbounds keep iterating through straight_dirs, otherwise move on to the next position
+            while new_x.between?(0, 7) && new_y.between?(0, 7)
+                new_x = pos[0] + x
+                new_y = pos[1] + y
+                if @board[new_x][new_y].is_a?(NullPiece)      # checks to see if the new position is a NullPiece 
+                    moves << [new_x, new_y]                   
+                end 
+                if @board[new_x][new_y].color == self.color   # if same color, dont want to add the position to moves
+                    break
+                else                                          # if opponents color, include position and break
+                    moves << [new_x, new_y]
+                    break
+                end
+            end
+        end
+        moves
     end
 end
